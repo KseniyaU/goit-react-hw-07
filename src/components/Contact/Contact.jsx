3fa2ced8af843513aed307e786fa97ee4bb1842a
KseniyaU <140713902+KseniyaU@users.noreply.github.com>
2024-02-23 10:50:49 +0200

    1

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import { IoPerson } from "react-icons/io5";
+import { IoCall } from "react-icons/io5";
+import { deleteContact } from '../../redux/contactsSlice.js'
+import { useDispatch } from 'react-redux'
+
+
+import css from '../Contact/Contact.module.css'
+
+export const Contact = ({ contact: { id, name, number } }) => {
+      const dispatch = useDispatch();
+
+    const onDeleteContact = (id) => {
+    dispatch(deleteContact(id));
+  }
+    
+    return (
+        <>
+            <div>
+                <p><IoPerson />  { name}</p>
+                <p><IoCall />  {number}</p>
+            </div>
+            <button onClick={() => onDeleteContact(id)} type="button" className={ css.btnDelete}>Delete</button>
+        </>
+    )
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,30 @@
+import { Contact } from '../Contact/Contact'
+import css from '../ContactList/ContactList.module.css'
+import { useSelector } from 'react-redux'
+
+
+export const ContactList = () => {
+    // console.log(typeof(AllContacts));
+    const contacts = useSelector(state => state.contacts.abs);
+    const filterValue = useSelector(state => state.filters);
+ 
+
+
+    const allContacts = contacts.filter(user =>
+    user.name.toLowerCase().includes(filterValue.toLocaleLowerCase())
+    )
+
+  
+    return (<div>
+        <ul className={ css.allContacts}>
+            {
+                allContacts.map(value => (
+                    
+                    <li className={css.contactContainet} key={ value.id}>
+                         <Contact  contact={value} />
+                    </li>
+            )) }
+        
+        </ul>
+    </div>
+)}
